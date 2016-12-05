1de370b4ffe41cfc9a412fb831e6b1928ee3e88b
Swapnil <swapnil1988@gmail.com>
2016-12-05 12:01:18 +0800

    Multhithreading task

--- src/main/java/com/epam/concurrency/task/AccidentDataProcessor.java
+++ src/main/java/com/epam/concurrency/task/AccidentDataProcessor.java
@@ -1,11 +1,18 @@
 package com.epam.concurrency.task;
 
-import com.epam.data.RoadAccident;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.epam.data.RoadAccident;
 
 /**
  * Created by Tanmoy on 6/17/2016.
@@ -20,10 +27,15 @@ public class AccidentDataProcessor {
     private static final String OUTPUT_FILE_PATH = "target/DfTRoadSafety_Accidents_consolidated.csv";
 
     private static final int DATA_PROCESSING_BATCH_SIZE = 10000;
+    
+    BlockingQueue<List<RoadAccident>> roadAccidentQueue = new ArrayBlockingQueue<>(3);
+    BlockingQueue<List<RoadAccidentDetails>> roadAccidentDetailsQueue = new ArrayBlockingQueue<>(3);
+    private volatile boolean running = true;
 
     private AccidentDataReader accidentDataReader = new AccidentDataReader();
     private AccidentDataEnricher accidentDataEnricher = new AccidentDataEnricher();
     private AccidentDataWriter accidentDataWriter = new AccidentDataWriter();
+    
 
     private List<String> fileQueue = new ArrayList<String>();
 
@@ -43,30 +55,87 @@ public class AccidentDataProcessor {
         for (String accidentDataFile : fileQueue){
             log.info("Starting to process {} file ", accidentDataFile);
             accidentDataReader.init(DATA_PROCESSING_BATCH_SIZE, accidentDataFile);
-            processFile();
+            try {
+				processFile();
+			} catch (InterruptedException e) {
+				System.out.println("Existing system due to interruption!!");
+			}
         }
     }
 
-    private void processFile(){
-        int batchCount = 1;
-        while (!accidentDataReader.hasFinished()){
-            List<RoadAccident> roadAccidents = accidentDataReader.getNextBatch();
-            log.info("Read [{}] records in batch [{}]", roadAccidents.size(), batchCount++);
-            List<RoadAccidentDetails> roadAccidentDetailsList = accidentDataEnricher.enrichRoadAccidentData(roadAccidents);
-            log.info("Enriched records");
-            accidentDataWriter.writeAccidentData(roadAccidentDetailsList);
-            log.info("Written records");
-        }
+    private void processFile() throws InterruptedException{
+    	ExecutorService executor = Executors.newFixedThreadPool(3);
+    	FutureTask<RoadAccident> readerTaskFuture = new FutureTask<RoadAccident>(new Runnable() {
+					@Override
+					public void run() {
+						int batchCount = 1;
+						while (!accidentDataReader.hasFinished()) {
+							try {
+								List<RoadAccident> roadAccidents = accidentDataReader.getNextBatch();
+								roadAccidentQueue.put(roadAccidents);
+								log.info("Read [{}] records in batch [{}]", roadAccidents.size(), batchCount++);
+							} catch (InterruptedException e) {
+								running = false;
+							}
+						}
+					}
+				}, null);
+		executor.execute(readerTaskFuture);
+		
+		FutureTask<RoadAccidentDetails> enrichTaskFuture = new FutureTask<RoadAccidentDetails>(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (running) {
+						List<RoadAccidentDetails> roadAccidentDetailsList = accidentDataEnricher.enrichRoadAccidentData(roadAccidentQueue
+										.take());
+						roadAccidentDetailsQueue.put(roadAccidentDetailsList);
+						log.info("Enriched records");
+					}
+				} catch (InterruptedException e) {
+					running = false;
+				}
+				
+			}
+		}, null);
+		executor.execute(enrichTaskFuture);
+		
+		FutureTask<RoadAccidentDetails> writerTaskFuture = new FutureTask<RoadAccidentDetails>(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (running) {
+						accidentDataWriter.writeAccidentData(roadAccidentDetailsQueue.take());
+						log.info("Written records");
+					}
+				} catch (InterruptedException e) {
+					running = false;
+				}
+			}
+		}, null);
+		executor.execute(writerTaskFuture);
+		
+		try {
+			if(readerTaskFuture.get() != null || !running){
+				readerTaskFuture.cancel(true);
+				enrichTaskFuture.cancel(true);
+				writerTaskFuture.cancel(true);
+				executor.shutdownNow();
+			}
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
     }
-
-
+    
+    
     public static void main(String[] args) {
         AccidentDataProcessor dataProcessor = new AccidentDataProcessor();
         long start = System.currentTimeMillis();
+        System.out.println("Start : " + start/1000);
         dataProcessor.init();
         dataProcessor.process();
         long end = System.currentTimeMillis();
+        System.out.println("end : " + end/1000);
         System.out.println("Process finished in s : " + (end-start)/1000);
     }
-
 }
